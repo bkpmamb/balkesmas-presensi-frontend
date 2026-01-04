dbea99747781514b2576a620554ee0da3eda291e
alif7laksono <alif7laksono@gmail.com>
2026-01-05 06:21:09 +0700

    add config --global user.email

--- src/app/dashboard/employees/[id]/page.tsx
+++ src/app/dashboard/employees/[id]/page.tsx
@@ -49,8 +49,6 @@ export default function EmployeeDetailPage({
     return <EmployeeNotFound />;
   }
 
-  // just added a web hook
-
   return (
     <div className="space-y-6">
       <EmployeeDetailHeader
