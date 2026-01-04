97627331f6130bb17663870947156388e715c825
alif7laksono <alif7laksono@gmail.com>
2026-01-05 06:04:53 +0700

    add config user.email

--- src/app/dashboard/employees/[id]/page.tsx
+++ src/app/dashboard/employees/[id]/page.tsx
@@ -49,8 +49,6 @@ export default function EmployeeDetailPage({
     return <EmployeeNotFound />;
   }
 
-  // accept invitation
-
   return (
     <div className="space-y-6">
       <EmployeeDetailHeader
